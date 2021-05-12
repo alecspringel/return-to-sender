42ddf7a94387b32c9c864bbc17c7b20fb0f6e074
Alec Springel <alecspringel@gmail.com>
2021-05-11 22:30:02 -0700

    Added controls menu and updated script

--- Assets/Scripts/MainMenu/MenuButtons.cs
+++ Assets/Scripts/MainMenu/MenuButtons.cs
@@ -5,14 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    public bool newGame;
-    public bool controls;
-    public bool exitGame;
+  public bool newGame;
+  public bool controls;
+  public bool exitGame;
+  public bool controlsBack;
+
+  public Camera MainCamera;
+  public Camera ControlsCamera;
+
+  void Awake()
+  {
+    MainCamera.enabled = true;
+    ControlsCamera.enabled = false;
+  }
   void OnMouseUp()
   {
     if (newGame)
     {
       SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+    if (controls)
+    {
+      MainCamera.enabled = false;
+      ControlsCamera.enabled = true;
+    }
+    if (controlsBack)
+    {
+      MainCamera.enabled = true;
+      ControlsCamera.enabled = false;
     }
     if (exitGame)
     {
