3d651b49a302abf6f6c21a48bd8108c56c202f87
Brandon Dodd <legendarysandbird@gmail.com>
2021-06-03 23:59:44 -0700

    Get jump sound effect set up

--- Assets/Scripts/NewPlayerMovement.cs
+++ Assets/Scripts/NewPlayerMovement.cs
@@ -11,6 +11,7 @@ public class NewPlayerMovement : MonoBehaviour
     public float clockTime;
     public bool pauseTimer = false;
     public AudioSource music;
+    public AudioSource jumpSound;
 
     private Animator animator;
     private Vector3 movement;
@@ -78,6 +79,8 @@ public class NewPlayerMovement : MonoBehaviour
             {
                 jumpTimer = 0.0f;
                 rigid.AddForce(new Vector3(0.0f, jumpHeight, 0.0f));
+                jumpSound.pitch = Random.Range(.8f, 1.2f);
+                jumpSound.Play();
             }
             jump = false;
         }
